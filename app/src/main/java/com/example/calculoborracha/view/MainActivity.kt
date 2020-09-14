7974d8932bb86c49de5f90f597c7d17034202a36
Juarez Lopes <juarezlopesfjr@gmail.com>
2020-09-14 20:37:35 -0300

    Added dark mode style

--- app/src/main/java/com/example/calculoborracha/view/MainActivity.kt
+++ app/src/main/java/com/example/calculoborracha/view/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.calculoborracha.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 
         // config do botao de navegar pagina anterior no topo da tela
         navController = Navigation.findNavController(this, R.id.fragment)
